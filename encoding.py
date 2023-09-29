c64ac4ac43922d354ccc41e92c357aed2258b4e2
birendra401 <82898373+skcvdev@users.noreply.github.com>
2023-09-29 22:32:38 +0545

    Update encoding.py

--- encoding.py
+++ encoding.py
@@ -1,2 +1,16 @@
-print('birenra')
-print('ram eats rice')
+# Text to encode
+original_text = "Hello, World! 🌍"
+
+# Encode the text to UTF-8
+encoded_text = original_text.encode('utf-8')
+
+# Print the encoded bytes
+print("Encoded Text (UTF-8):")
+print(encoded_text)
+
+# Decode the UTF-8 bytes back to text
+decoded_text = encoded_text.decode('utf-8')
+
+# Print the decoded text
+print("\nDecoded Text:")
+print(decoded_text)
